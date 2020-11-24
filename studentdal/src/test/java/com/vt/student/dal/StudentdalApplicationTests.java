f095cb9b59dc813a9da2a69acee6ae49de70f386
vt92 <52454990+vt92@users.noreply.github.com>
2020-11-24 16:31:05 +0530

    Update StudentdalApplicationTests.java

--- studentdal/src/test/java/com/vt/student/dal/StudentdalApplicationTests.java
+++ studentdal/src/test/java/com/vt/student/dal/StudentdalApplicationTests.java
@@ -1,5 +1,7 @@
 package com.vt.student.dal;
 
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,12 +19,30 @@ class StudentdalApplicationTests {
 	@Test
 	public void testCreateStudent() {
 		Student student =new Student();
-		student.setId(1L);
-		student.setName("John");
-		student.setCourse("Web Services");
-		student.setFee(30D);
+		student.setId(2L);
+		student.setName("Jon");
+		student.setCourse("RestWeb Services");
+		student.setFee(35D);
 		
 		studentRepository.save(student);
 	}
 
+	@Test
+	public void testFindStudentById() {
+		Optional<Student> student = studentRepository.findById(1L);
+		System.out.println(student);
+	}
+	
+	@Test
+	public void testUpdateStudent() {
+		Optional<Student> student = studentRepository.findById(1L);
+		//we will have to use ".get" as we are using optional
+		student.get().setName("John-Updated"); 
+		studentRepository.save(student.get());	
+	}
+	
+	@Test
+	public void testDeleteStudent() {
+		studentRepository.deleteById(1L);
+	}
 }
